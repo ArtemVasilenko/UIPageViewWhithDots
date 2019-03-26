937b771dc1c5b0cbe923f708743dcbb228a4b0d1
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-03-26 22:20:27 +0200

    start

---
+++ UIPageViewWhithDots/MyVC.swift
@@ -0,0 +1,30 @@
+//
+//  MyVC.swift
+//  UIPageViewWhithDots
+//
+//  Created by Артем on 3/26/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class MyVC: UIViewController {
+    
+    @IBOutlet weak var contView: UIView!
+    @IBOutlet weak var dots: UIPageControl!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let myPageVC = segue.destination as! MyPageViewController
+//        myPageVC.myDelegate = self
+        
+    }
+
+}
